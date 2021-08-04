e9319f3fa5f13850897526a6ec701bd13dc86de0
Your Name <you@example.com>
2021-08-04 15:08:35 +0530

    refactor: player position

---
+++ Snake_Ladder/Snake_Ladder/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Snake_Ladder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a Snake and Ladder Problem");
+            int PlayerOne = 1;
+            int PlayerPosition = 0;
+
+        }
+    }
+}
